c9d9815b8c5eb481de0d927e277dcb76c13a96cc
Radu BOBES <radu-bobes_student2023@wilder.school>
2023-04-18 11:54:55 +0200

    first commit

---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+
+require __DIR__ . '/../src/Hello.php';
+
+use App\Hello;
---
+++ src/Hello.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+class Hello
+{
+
+public function speak(): string
+    {
+        return 'Hello World!';
+    }
+}
